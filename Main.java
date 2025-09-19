1809c947fe159f28eda6df4a75dc920e6f8bd302
davidl572 <davidl572@nycstudents.net>
2025-09-19 03:21:43 -0400

    Added test cases

--- Main.java
+++ Main.java
@@ -10,6 +10,8 @@ public class Main
         f = new Feeder(1000);
         f.simulateOneDay(22);
         System.out.println("Current Food: " + f.getCurrentFood());
+        f = new Feeder(250);
+        System.out.println(f.simulateManyDays(10,5) + " days");
         f = new Feeder(0);
         System.out.println(f.simulateManyDays(5,10) + " days");
     }
